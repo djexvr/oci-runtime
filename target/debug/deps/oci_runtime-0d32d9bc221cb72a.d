ce8f950a27fdb565f867c91704a7fb8dae58fa8f
Vianney Ruhlmann <ruhlmannvianney@gmail.com>
2023-05-10 16:39:17 +0200

    Add gitignore

--- target/debug/deps/oci_runtime-0d32d9bc221cb72a.d
+++
@@ -1,5 +0,0 @@
-/home/byejablek/Documents/2A/Programmation/oci-runtime/target/debug/deps/oci_runtime-0d32d9bc221cb72a: src/main.rs
-
-/home/byejablek/Documents/2A/Programmation/oci-runtime/target/debug/deps/oci_runtime-0d32d9bc221cb72a.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/oci_runtime-a5fb544d94f888f2.d
+++
@@ -1,7 +0,0 @@
-/home/byejablek/Documents/2A/Programmation/oci-runtime/target/debug/deps/oci_runtime-a5fb544d94f888f2.rmeta: src/lib.rs
-
-/home/byejablek/Documents/2A/Programmation/oci-runtime/target/debug/deps/liboci_runtime-a5fb544d94f888f2.rlib: src/lib.rs
-
-/home/byejablek/Documents/2A/Programmation/oci-runtime/target/debug/deps/oci_runtime-a5fb544d94f888f2.d: src/lib.rs
-
-src/lib.rs:
